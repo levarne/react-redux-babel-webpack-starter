1b7b26001d97dd13c4005be1e0bebef42976d0d8
levarne <levarne.sobotker@manalto.com>
2017-08-04 01:38:08 +0200

    starter app setup

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# react-redux-babel-webpack-starter
-This is a starter app using REACT REDUX BABEL WEBPACK
+# react redux babel webpack starter app
+This is a starter app using REACT REDUX-FLUX PATTERN BABEL WEBPACK
+
+
+### Global Dependencies
+``` sh
+$cd yarn add babel-cli -g
+```
+
+### Setup Project
+``` sh
+$cd yarn install
+```
+
+### Run Project
+``` sh
+$cd yarn run build
+
+or
+
+$cd yarn run serve
+```
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+	entry: './src/main.jsx',
+	output: {
+		filename: 'build.js',
+		path: path.resolve(__dirname, 'bin')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/,
+				enforce: "pre",
+				include: [
+					path.resolve(__dirname, "src"),
+				], // only files inside src
+				use: [
+					{
+						loader: "jshint-loader"
+					}
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [{
+					loader: 'style-loader'
+				},{
+					loader: 'css-loader'
+				},{
+					loader: 'sass-loader'
+				}]
+			},
+			{
+				test: /\.jsx$/,
+				include: [
+					path.resolve(__dirname, "src"),
+				],
+				loader: 'babel-loader',
+				query: {
+					presets: ['env', 'react']
+				}
+			}
+		]
+	},
+	watch: true,
+	plugins: [
+		new BrowserSyncPlugin({
+			host: 'localhost',
+			port: 3000,
+			files: ['./*.html', './bin/*.js'],
+			server: { baseDir: ['./'] }
+		},
+		{
+			reload: false
+		})
+	],
+};
